a8ffdd69217488a6907347c8c6b5f197cab50888
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-09-12 11:52:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var api = "http://aio.mine2.live:8117";
+var discord = "https://discord.gg/yxBhpAY";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.avrio.mine2.live/?hash={id}#blockchain_block";
+var transactionExplorer = "https://explorer.avrio.mine2.live/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
