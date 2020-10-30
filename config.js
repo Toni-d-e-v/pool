769186d922e76fc364aca6e51ede0483099178fa
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-10-30 16:11:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-var api = "http://aio.mine2.live:8117";
+var api = "http://178.236.85.196/:44402";
 var discord = "https://discord.gg/yxBhpAY";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "https://explorer.avrio.mine2.live/?hash={id}#blockchain_block";
-var transactionExplorer = "https://explorer.avrio.mine2.live/?hash={id}#blockchain_transaction";
+var blockchainExplorer = "http://178.236.85.196/?hash={id}#blockchain_block";
+var transactionExplorer = "http://178.236.85.196/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
